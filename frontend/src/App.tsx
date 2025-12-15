e703a843b357f5ab9b86f137734057a67c5c952e
ascandone <alescandone@gmail.com>
2025-12-15 09:01:55 +0100

    implement basic UI for upload

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,8 @@
-import type { FC } from "react";
+import { type FC } from "react";
+import ProgressUpload from "./components/file-upload/progress-upload";
 
-export const App: FC = () => <div>Hi!</div>;
+export const App: FC = () => (
+  <div className="mx-auto max-w-lg py-12">
+    <ProgressUpload />
+  </div>
+);
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,13 @@
+import path from "path";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
