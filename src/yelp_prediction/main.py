e7afe9bf32b6584092eef2e2631b3644560e72f6
ascandone <alescandone@gmail.com>
2025-12-18 17:52:19 +0100

    remove unused param

--- src/yelp_prediction/main.py
+++ src/yelp_prediction/main.py
@@ -5,6 +5,5 @@ if __name__ == "__main__":
     features = torch.load("./data/features/features-clip.pt")
     train_features.run(
         features_dict=features,
-        max_photos=5,
         input_dim=512,
     )
